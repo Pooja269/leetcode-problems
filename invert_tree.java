e972ac2b1632d32982e2e7507e5ae5802f498117
pooja <Pooja269@users.noreply.github.com>
2020-06-01 23:34:07 +0530

    Create invert_tree.java

---
+++ invert_tree.java
@@ -0,0 +1,41 @@
+/*Invert a binary tree.
+input:
+[4,2,7,1,3,6,9]
+Output:
+[4,7,2,9,6,3,1]
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        
+        if(root==null){
+            return null;
+        }
+        Queue<TreeNode> q=new LinkedList<TreeNode>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            TreeNode node=q.poll();
+            TreeNode temp=node.left;
+            node.left=node.right;
+            node.right=temp;
+            if(node.left!=null) q.add(node.left);
+            if(node.right!=null) q.add(node.right);
+        }
+        return root;
+    }
+    
+}
