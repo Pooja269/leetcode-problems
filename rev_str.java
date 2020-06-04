060806e3ccd3270ec950e739613d90012eab6f9d
pooja <Pooja269@users.noreply.github.com>
2020-06-04 13:10:11 +0530

    reverse a string using two pointer logic

---
+++ rev_str.java
@@ -0,0 +1,22 @@
+/*reverse a string with O(1) space complexity
+Example 1:
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Example 2:
+Input: ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+*/
+class Solution {
+    public void reverseString(char[] s) {
+        int i=0;
+        int j=s.length-1;
+        while(i<j){
+            char temp=s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            i++;
+            j--;
+        }
+    }
+}
